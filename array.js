9f83af37c25d2230ca23073542187547270c23b3
Fernando Rodriguez <ferrod20@gmail.com>
2013-12-11 17:55:52 -0300

    remove contains last & find methods added

--- array.js
+++ array.js
@@ -1,13 +1,15 @@
 /*
 *
-* arraySet.js
+* array.js
 * https: //github.com/ferrod20/array.js
 *
-* Set methods for array
+* Useful javascript array methods
 *
+* This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY
+* 
 * version: 0.1 (alpha)
 * Created by Fernando Rodriguez
-*
+* 
 */
 
 if (!Array.prototype.subtract) {
@@ -33,5 +35,32 @@ if (!Array.prototype.intersect) {
 if (!Array.prototype.exclusion) {
 	Array.prototype.exclusion=function(arr, fun){ 
 		return this.subtract(arr, fun).concat(arr.subtract(this, fun));
+    };
+};
+
+if (!Array.prototype.remove) {
+    Array.prototype.remove = function (element) {
+        var index = this.indexOf(element);
+        if (index !== -1)
+            this.splice(index, 1);
+    };
+};
+
+if (!Array.prototype.contains) {
+    Array.prototype.contains = function (element) {
+        return this.indexOf(element) > -1;        
+    };
+};
+
+if (!Array.prototype.last) {
+    Array.prototype.last = function () {
+        return this[this.length - 1];        
+    };
+};
+
+if (!Array.prototype.find) {
+    Array.prototype.find = function (fun) {
+    	var filtered = this.filter(fun);
+        return filtered.length ? filtered[0] : null;
     };
 };
